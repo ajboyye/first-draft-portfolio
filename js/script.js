4f0e23978a885f6b11b8fb53f88af39f970fbb75
ajboyye <agasalexander08@gmail.com>
2026-05-06 12:27:13 -1000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-6767
+// This shows a message in the browser's console (F12 to see it)
+console.log("Portfolio loaded successfully!");
+
+// This adds a simple greeting based on the time of day
+const hour = new Date().getHours();
+let greeting;
+
+if (hour < 12) {
+  greeting = "Good Morning!";
+} else if (hour < 18) {
+  greeting = "Good Afternoon!";
+} else {
+  greeting = "Good Evening!";
+}
+
+// This will pop up when someone first visits your site
+alert(greeting + " Welcome to my electronic portfolio.");
